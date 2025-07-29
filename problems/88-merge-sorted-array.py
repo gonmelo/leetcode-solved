a7cb2ecac51245e7c7ff877633c9fa6f815706a0
Goncalo Melo <gonvm1@gmail.com>
2025-07-29 22:48:30 +0100

    feat: solve problem 88

--- problems/88-merge-sorted-array.py
+++ problems/88-merge-sorted-array.py
@@ -1,10 +1,37 @@
 """
 88. Merge Sorted Array
 Link: https://leetcode.com/problems/merge-sorted-array/
-Difficulty: TODO
+Difficulty: Medium
 
 Approach:
-- TODO: describe the algorithm
-- Time Complexity: TODO
-- Space Complexity: TODO
+- We use a two-pointer technique to merge the two arrays in reverse order.
+- Time Complexity: O(m + n)
+- Space Complexity: O(1)
 """
+
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        p1 = m - 1
+        p2 = n - 1
+
+        p = m + n - 1
+        while p >= 0:
+            if p2 < 0:
+                break
+
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+
+            p -= 1
+            
