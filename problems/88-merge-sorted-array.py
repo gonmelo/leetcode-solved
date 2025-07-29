32d6adbdd4cc81587ac61b1b93c7fe9247f30e1d
Goncalo Melo <gonvm1@gmail.com>
2025-07-29 22:31:35 +0100

    feat: solve problem 88

---
+++ problems/88-merge-sorted-array.py
@@ -0,0 +1,10 @@
+"""
+88. Merge Sorted Array
+Link: https://leetcode.com/problems/merge-sorted-array/
+Difficulty: TODO
+
+Approach:
+- TODO: describe the algorithm
+- Time Complexity: TODO
+- Space Complexity: TODO
+"""
